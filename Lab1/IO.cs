751c9bd283644e7ef2ce197f5f780586b98dff3a
Molodetskiy Arseniy <molodeckij.b.2017@fml31.ru>
2018-10-16 19:54:13 +0300

    Refactored exceptions and added io to split input outpu and logic

---
+++ Lab1/IO.cs
@@ -0,0 +1,137 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace Lab1
+{
+    public class IO
+    {
+        public Matrix InputMatrixDialog(int maxMatrixHeight)
+        {
+            var k = AskCount(maxMatrixHeight);
+            ShowMenu();
+
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    return ConsoleInput(k);
+                case "2":
+                    return FileInput(k);
+                case "3":
+                    return RandomInput(k);
+                default:
+                    throw new InvalidDataException("Ошибка: выберите число в диапазоне от 1 до 3");
+            }
+        }     
+
+        private int AskCount(int maxMatrixHeight)
+        {
+            do
+            {
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Введите размер матрицы k");
+                var isNumber = int.TryParse(Console.ReadLine(), out var k);
+                if (isNumber && k > 0)
+                {
+                    if (k > maxMatrixHeight)
+                        Console.WriteLine($"Ошибка: K должно быть <= {maxMatrixHeight}");
+                    else
+                        return k;
+                }
+                else
+                {
+                    Console.WriteLine("Ошиба: K неотрицательно");
+                }
+            } while (true);
+        }
+
+        private void ShowMenu()
+        {
+            Console.WriteLine("Нажмите соответствующую клавишу для выбора:");
+            Console.WriteLine("1) Задать из консоли");
+            Console.WriteLine("2) Задать из файла");
+            Console.WriteLine("3) Задать случайно");
+        }
+
+        private Matrix RandomInput(int k)
+        {
+            double min, max;
+            var isInputParamsCorrect = false;
+            do
+            {
+                Console.WriteLine("Введите минимальное значение");
+                try
+                {
+                    min = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите максимальное значение");
+                    max = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return null;
+                }
+
+                if (!(min > max) && !NumericComparer.Compare(min, max))
+                {
+                    isInputParamsCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Максимальное значение должно быть строго больше минимального");
+                }
+            } while (!isInputParamsCorrect);
+
+            var randomGenerator = new RandomMatrixGenerator();
+            return new Matrix(randomGenerator.Generate(k, min, max));    
+
+        }
+
+        private Matrix FileInput(int k)
+        {
+            Console.WriteLine("Введите путь к файлу");
+            var fileName = Console.ReadLine();
+            if (fileName == null || !File.Exists(fileName))
+            {
+                Console.WriteLine("Файл не существует");
+                return null;
+            }
+
+            using (var sr = new StreamReader(fileName))
+            {
+                var source = sr.ReadToEnd();
+                return HandleMatrixParse(source, k);
+            }
+        }
+
+        private Matrix ConsoleInput(int k)
+        {
+            var matrixString = new StringBuilder();
+            for (var i = 0; i < k; i++)
+            {
+                Console.WriteLine($"Введите коэффициенты уравнения №{i+1} ");
+                matrixString.Append(Console.ReadLine());
+                matrixString.Append(' ');
+            } 
+            return HandleMatrixParse(matrixString.ToString(), k);
+            
+        }
+
+        private Matrix HandleMatrixParse(string source, int k)
+        {
+            Matrix result;
+            try
+            {
+                result = Matrix.Parse(source, k);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,10 +10,21 @@ namespace Lab1
 
         public static void Main(string[] args)
         {
-            Matrix matrix;
+            var io = new IO();
+
+            Matrix matrix = null;
             do
             {
-                matrix = InputMatrix();
+                try
+                {
+                    matrix = io.InputMatrixDialog(MaxMatrixHeight);
+                }
+                catch (InvalidDataException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+               
+                
             } while (matrix == null);
 
 
@@ -31,148 +42,13 @@ namespace Lab1
                 var errors = matrix.GetErrors(solutions);
                 Console.WriteLine("Решения и невязки");
                 for (var i = 0; i < solutions.Length; i++)
-                    Console.WriteLine($"X{i+1}: {solutions[i]:0.#######} | error: {errors[i]} ");
+                    Console.WriteLine($"X{i+1}: {solutions[i]} | error: {errors[i]} ");
             }
 
             Console.Write("Определитель матрицы: ");
             Console.WriteLine(matrix.GetDeterminant().ToString("0.#######"));
         }
         
-        private static Matrix InputMatrix()
-        {
-            var k = AskCount();
-            ShowMenu();
-
-            var choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "1":
-                    return ConsoleInput(k);
-                case "2":
-                    return FileInput(k);
-                case "3":
-                    return RandomInput(k);
-                default:
-                    throw new InvalidDataException();
-            }
-        }
-
-        private static int AskCount()
-        {
-            do
-            {
-                Console.WriteLine("-------------------------------------------");
-                Console.WriteLine("Введите размер матрицы k");
-                var isInputCorrect = int.TryParse(Console.ReadLine(), out var k);
-                if (isInputCorrect && k > 0)
-                {
-                    if (k > MaxMatrixHeight)
-                        Console.WriteLine($"K должно быть <= {MaxMatrixHeight}");
-                    else
-                        return k;
-                }
-                else
-                {
-                    Console.WriteLine("K неотрицательно");
-                }
-            } while (true);
-        }
-
-        private static void ShowMenu()
-        {
-            Console.WriteLine("Нажмите соответствующую клавишу для выбора:");
-            Console.WriteLine("1) Задать из консоли");
-            Console.WriteLine("2) Задать из файла");
-            Console.WriteLine("3) Задать случайно");
-        }
-
-        private static Matrix RandomInput(int k)
-        {
-            double min, max;
-            var isInputParamsCorrect = false;
-            do
-            {
-                Console.WriteLine("Введите минимальное значение");
-                try
-                {
-                    min = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Введите максимальное значение");
-                    max = double.Parse(Console.ReadLine());
-                }
-                catch (FormatException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    return null;
-                }
-
-                if (!(min > max) && !NumericComparer.Compare(min, max))
-                {
-                    isInputParamsCorrect = true;
-                }
-                else
-                {
-                    Console.WriteLine("Максимальное значение должно быть строго больше минимального");
-                }
-            } while (!isInputParamsCorrect);
-            
-            return new Matrix(RandomGenerator(k, min, max));    
-
-        }
-
-        private static Matrix FileInput(int k)
-        {
-            Console.WriteLine("Введите путь к файлу");
-            var fileName = Console.ReadLine();
-            if (fileName == null || !File.Exists(fileName))
-            {
-                Console.WriteLine("Файл не существует");
-                return null;
-            }
-
-            using (var sr = new StreamReader(fileName))
-            {
-                var source = sr.ReadToEnd();
-                return HandleMatrixParse(source, k);
-            }
-        }
-
-        private static Matrix ConsoleInput(int k)
-        {
-            var matrixString = new StringBuilder();
-            for (var i = 0; i < k; i++)
-            {
-                Console.WriteLine($"Введите коэффициенты уравнения №{i+1} ");
-                matrixString.Append(Console.ReadLine());
-                matrixString.Append(' ');
-            }
-            return HandleMatrixParse(matrixString.ToString(), k);
-            
-        }
-
-        private static double[,] RandomGenerator(int size, double min, double max)
-        {
-            var gen = new Random();
-            var randomMatrix = new double[size, size + 1];
-            for (var i = 0; i < size; i++)
-            for (var j = 0; j < size + 1; j++)
-                randomMatrix[i, j] = gen.NextDouble() * (max - min) + min;
-            return randomMatrix;
-        }
-
-        private static Matrix HandleMatrixParse(string source, int k)
-        {
-            Matrix result;
-            try
-            {
-                result = Matrix.Parse(source, k);
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e);
-                return null;
-            }
-
-            return result;
-        }
+        
     }
 }
