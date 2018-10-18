060347404a288b5997e18226422eb1fdca154af6
Orfac <molodeckij.b.2017@fml31.ru>
2018-10-18 22:07:18 +0300

    made NumbericComparer to static

--- Lab2/NumericComparer.cs
+++ Lab2/NumericComparer.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Lab2
 {
-    public class NumericComparer
+    public static class NumericComparer
     {
         public static bool Compare(double x, double y)
         {
