ab16b5d8e5d1da0b462e907853467a9c656fabc9
arseniy <molodeckij.b.2017@fml31.ru>
2018-12-03 01:54:15 +0300

    added Parser for parsing string to array of dots

---
+++ Lab3/Services/Parsers/Parser.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Lab3.Services.Parsers
+{
+    public class Parser : IParser
+    {
+        public double[] parseArray(string input)
+        {
+            input = input.Replace(',','.');
+            var elements = input.Split(new[] {' ', '\t', '\n', '\r'},
+                StringSplitOptions.RemoveEmptyEntries);
+
+            double[] parsedArray = new double[elements.Length];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                parsedArray[i] = double.Parse(elements[i]);
+            }
+
+            return parsedArray;
+        }
+    }
+}
