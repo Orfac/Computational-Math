8989122e847d115585bbc18a0d8173735aeb5db3
arseniy <molodeckij.b.2017@fml31.ru>
2018-12-15 22:03:09 +0300

    refactored naming

--- Lab3/Models/Methods/LagrangeMethod.cs
+++ Lab3/Models/Methods/LagrangeMethod.cs
@@ -14,12 +14,17 @@ namespace Lab3.Models.Methods
             
             int size = xData.Length;
             double sum = 0; 
-            for(int i = 0; i < size; i++){
-                double mul = 1; 
-                for(int j = 0; j < size; j++){
-                    if(i!=j) mul *= (x - xData[j])/(xData[i]-xData[j]);
+            for(int i = 0; i < size; i++)
+            {
+                double multiplier = 1; 
+                for(int j = 0; j < size; j++)
+                {
+                    if(i!=j) 
+                    {
+                        multiplier *= (x - xData[j])/(xData[i]-xData[j]);
+                    }
                 }
-                sum += yData[i]*mul;
+                sum += yData[i]*multiplier;
             }
             return sum;
         }
